9d4426f1dd9313ed5318f9db53c7b8f3fdf3b59f
amran-zak <amranabdourazak@gmail.com>
2023-11-24 16:04:45 +0100

    test de prof

--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
 import { MarsRover } from "./MarsRover";
 
-const rover = new MarsRover(0, 0, 'N');
-console.log(rover.getState()); // Output: "(0, 0), Orientation: N"
-rover.turnLeft();
-console.log(rover.getState());
-
+const rover = new MarsRover(1, 1, 'N');
+rover.moveForward();
+rover.turnRight();
 rover.moveBackward();
-console.log(rover.getState());
+rover.turnRight();
+rover.moveForward();
+rover.turnRight();
+rover.moveForward()
+console.log(rover.getState()); 
 
-rover.turnLeft();
-console.log(rover.getState());
 
 function sayHello(): void {
   console.log("Hello, World!");
