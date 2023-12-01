dec859af2333bc84e6c74fb4b8e0fcdb1686961d
perrineosw <oswald.perrinedu40@gmail.com>
2023-12-01 12:21:53 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,19 +2,7 @@ import { Rover } from "./classes/Rover";
 import { Orientation } from "./enums/Orientation";
 import { Map } from "./classes/Map";
 
-const map: Map = {
-  x: 4,
-  y: 4,
-  getSize: function (): { x: number; y: number } {
-    return { x: this.x, y: this.y };
-  },
-	getToroidalCoordinates: function (x: number, y: number): { x: number; y: number } {
-    return {
-      x: (x + this.x) % this.x,
-      y: (y + this.y) % this.y,
-    };
-  },
-};
+const map = new Map(4, 4);
 
 const rover = new Rover(Orientation.South, { x: 0, y: 0 }, map);
 
