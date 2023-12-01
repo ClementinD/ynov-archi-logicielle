7f2c41a9473e6167f1a0283ffaf7253acaa66ade
perrineosw <oswald.perrinedu40@gmail.com>
2023-12-01 12:17:32 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,11 +3,17 @@ import { Orientation } from "./enums/Orientation";
 import { Map } from "./classes/Map";
 
 const map: Map = {
-	x: 4,
-	y: 4,
-	getSize: function (): { x: number; y: number } | null {
-		throw new Error("Function not implemented.");
-	},
+  x: 4,
+  y: 4,
+  getSize: function (): { x: number; y: number } {
+    return { x: this.x, y: this.y };
+  },
+	getToroidalCoordinates: function (x: number, y: number): { x: number; y: number } {
+    return {
+      x: (x + this.x) % this.x,
+      y: (y + this.y) % this.y,
+    };
+  },
 };
 
 const rover = new Rover(Orientation.South, { x: 0, y: 0 }, map);
