42c40091bf205aff35948f781b426c3e8a8f81d1
amran-zak <amranabdourazak@gmail.com>
2023-12-22 15:18:51 +0100

    test

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ const obstaclesPositions = [new Position(2, 1)]; // Remplacer par les positions 
 // Créer une nouvelle instance de Rover en passant les obstacles comme quatrième argument
 const rover = new Rover(Orientation.South, new Position(0, 0), map, obstaclesPositions);
 
-const commandes = ["AA", "D", "R", "D", "A", "D", "A"];
+const commandes = ["A", "D", "R", "DAGADRGAAADR"];
 
 const interpreter = new Interpreter(rover);
 
