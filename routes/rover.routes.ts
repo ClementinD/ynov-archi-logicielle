ce2df52bea6c7d4236153a1478d5b65f2e49c3f8
jean <jeanclaverie@outlook.com>
2024-01-12 14:55:56 +0100

    clear code

--- routes/rover.routes.ts
+++ routes/rover.routes.ts
@@ -4,14 +4,10 @@ module.exports = (app: any, interpreter: any) => {
   const router = Router();
 
   router.post("/", (req: Request, res: Response) => {
-    console.log(interpreter.rover.position.value());
-
     const commandes = req.body.command;
     commandes.forEach((commande: Array<String>) => {
       interpreter.interpretCommand(commande);
     });
-
-    console.log(interpreter.rover.position.value());
 
     res.json({ position: interpreter.rover.position.value() });
   });
